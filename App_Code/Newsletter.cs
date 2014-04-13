5860900a3ccbcea0963f0744a5f4c8f1bece0044
misschiwawa <info@nadiabernard.com>
2014-04-12 21:16:50 -0400

    Update Newsletter.cs

--- App_Code/Newsletter.cs
+++ App_Code/Newsletter.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -76,3 +76,4 @@ public class Newsletter
         }
         #endregion
 }
+*/
